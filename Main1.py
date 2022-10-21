08edc740c35c4392b7eb9e240de8fb4eeb9f6e16
camillaoggioni <camilla1.oggioni@iisgalvanimi.edu.it>
2022-10-21 09:21:28 +0000

    v. 1.0

---
+++ Main1.py
@@ -0,0 +1,32 @@
+#Creazione variabili e stampa delle stesse
+x=18
+y="Camilla"
+print(x)
+print(y)
+#Riassegnazione del tipo della variabile x
+x=76        #x è una variabile int
+x="Rudolf"  #x ora è una variabile str
+print(x)
+#Per cambiare una variabile di un determinato tipo bisogna dischiararlo tramite il casting
+x=str(7)    #7 è una stringa '7'
+y=int(7)    #7 è un intero 7
+z=float(7)  #7 è un decimale 7.0
+#Scoprire il tipo di variabile
+x=54
+y="Gianpiero"
+print(type(x))
+print(type(y))
+#Le variabili possono essere dichiarate sia con una virgoletta(') che con due("")
+x='Bronag'
+x="Bronag"   #sono la stessa cosa
+#I nomi delle variabili vengono distinti tra minuscole  e maiuscole
+a="Roberto"
+A=4          #le variabili non si sovrascrivono
+#Il nome delle variabili può iniziare con una lettera o un carattere di sottolineatura, non può iniziare con un numero, può contenere solo caratteri alfanumerici e trattini bassi, distingue maiuscol e minuscole
+myvar = "Pingu"
+my_var = "Pingu"
+_my_var = " Pingu"
+myVar = "Pingu"
+MYVAR = "Pingu"
+myvar2 = "Pingu"
+#
